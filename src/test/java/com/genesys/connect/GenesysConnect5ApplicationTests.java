4982ecc2a5d60eb79a6eedbb9611cffa101b2f67
Neha Choudhary <neha@Neha.grindrod.in>
2020-03-25 21:21:28 +0000

    Committing all files

---
+++ src/test/java/com/genesys/connect/GenesysConnect5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesys.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesysConnect5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
